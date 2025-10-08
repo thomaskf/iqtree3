b563319e21bf7bf909055ebb6a29d2f90f56146f
Thomas Wong <thomaskf@gmail.com>
2025-10-08 13:09:40 +1100

    Fixed the inconsistent likelihood values during the branch length optimisation.

--- tree/phylotreebrmodel.cpp
+++ tree/phylotreebrmodel.cpp
@@ -352,11 +352,8 @@ void PhyloTreeBranchModel::computeTipPartialLikelihood() {
 }
 
 double PhyloTreeBranchModel::computeLikelihood(double *pattern_lh, bool save_log_value) {
-    ASSERT(root->isLeaf());
-    if (!current_it) {
-        current_it = (PhyloNeighbor*)root->neighbors[0];
-        current_it_back = (PhyloNeighbor*)current_it->node->findNeighbor(root);
-    }
+    current_it = (PhyloNeighbor*)root->neighbors[0];
+    current_it_back = (PhyloNeighbor*)current_it->node->findNeighbor(root);
     return PhyloTree::computeLikelihood(pattern_lh, save_log_value);
 }
 
