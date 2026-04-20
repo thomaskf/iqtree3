f6430f905f2f9690b987120c693261bf5a7749d8
NhanLT <trongnhan.uit@gmail.com>
2026-04-20 15:18:14 +1000

    specify a output dir for IQ-TREE 2 to avoid name clashes with IQ-TREE 3

--- test_scripts/test_iqtree.ps1
+++ test_scripts/test_iqtree.ps1
@@ -1,11 +1,15 @@
 param (
     [string]$IQTreeBin = "./build/iqtree3",
-    [string]$LogFile = "time_log.tsv"
+    [string]$LogFile   = "time_log.tsv",
+    [string]$OutDir    = "test_scripts/test_data"
 )
 
-$SEED=73073
-
+$SEED   = 73073
 $LOGFILE = $LogFile
+$WD     = "test_scripts/test_data"
+
+New-Item -ItemType Directory -Force -Path $OutDir | Out-Null
+
 "Command`tRealTime(s)`tPeakMemory(MB)" | Out-File -FilePath $LOGFILE -Encoding utf8
 
 function Measure-IQTree {
@@ -44,6 +48,12 @@ function Measure-IQTree {
     $endTime = Get-Date
     $elapsed = [math]::Round(($endTime - $startTime).TotalSeconds, 2)
 
+    if ($proc.ExitCode -ne 0) {
+        Write-Host "WARNING: command exited with code $($proc.ExitCode), logging 0 for time and memory"
+        $elapsed    = 0
+        $peakMemory = 0
+    }
+
     # Show output
     Get-Content $tempOut
 
@@ -54,68 +64,61 @@ function Measure-IQTree {
     Remove-Item $tempOut
 }
 
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -B 1000 -T 1 -seed $SEED --prefix $OutDir/turtle.fa"
 
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -p $WD/turtle.nex -B 1000 -T 1 -seed $SEED --prefix $OutDir/turtle.nex"
 
-# ./build/iqtree3 -s test_scripts/test_data/small.fa -p test_scripts/test_data/small.nex -m "MFP+MERGE" -T 1 -seed $SEED
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -p $WD/turtle.nex -B 1000 -T 1 -m MFP+MERGE -rcluster 10 --prefix $OutDir/turtle.merge -seed $SEED"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -B 1000 -T 1 -seed $SEED"
+Get-Content "$OutDir/turtle.fa.treefile", "$OutDir/turtle.nex.treefile" |
+    Set-Content "$OutDir/turtle.trees"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -p $OutDir/turtle.merge.best_scheme.nex -z $OutDir/turtle.trees -zb 10000 -au -n 0 --prefix $OutDir/turtle.test -seed $SEED -T 1"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -p test_scripts/test_data/turtle.nex -B 1000 -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -m GTR+F+I+R3+T -te $OutDir/turtle.trees -T 1 --prefix $OutDir/turtle.mix -seed $SEED"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -p test_scripts/test_data/turtle.nex -B 1000 -T 1 -m MFP+MERGE -rcluster 10 --prefix test_scripts/test_data/turtle.merge -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -p $OutDir/turtle.nex.best_scheme.nex -z $OutDir/turtle.trees -n 0 -wpl --prefix $OutDir/turtle.wpl -seed $SEED -T 1"
 
-Get-Content test_scripts/test_data/turtle.fa.treefile, test_scripts/test_data/turtle.nex.treefile |
-    Set-Content test_scripts/test_data/turtle.trees
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -p test_scripts/test_data/turtle.merge.best_scheme.nex -z test_scripts/test_data/turtle.trees -zb 10000 -au -n 0 --prefix test_scripts/test_data/turtle.test -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -S $WD/turtle.nex --prefix $OutDir/turtle.loci -T 1 -seed $SEED"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -m GTR+F+I+R3+T -te test_scripts/test_data/turtle.trees -T 1 --prefix test_scripts/test_data/turtle.mix -seed $SEED"
+Measure-IQTree "$IQTreeBin -t $OutDir/turtle.nex.treefile --gcf $OutDir/turtle.loci.treefile -s $WD/turtle.fa --scf 100 -seed $SEED -T 1 --prefix $OutDir/turtle.nex.cf"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -p test_scripts/test_data/turtle.nex.best_scheme.nex -z test_scripts/test_data/turtle.trees -n 0 -wpl --prefix test_scripts/test_data/turtle.wpl -seed $SEED -T 1"
-
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -S test_scripts/test_data/turtle.nex --prefix test_scripts/test_data/turtle.loci -T 1 -seed $SEED"
-
-Measure-IQTree "$IQTreeBin -t test_scripts/test_data/turtle.nex.treefile --gcf test_scripts/test_data/turtle.loci.treefile -s test_scripts/test_data/turtle.fa --scf 100 -seed $SEED -T 1"
-
-Measure-IQTree "$IQTreeBin -t test_scripts/test_data/turtle.fa.treefile --gcf test_scripts/test_data/turtle.loci.treefile -s test_scripts/test_data/turtle.fa --scf 100 -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -t $OutDir/turtle.fa.treefile --gcf $OutDir/turtle.loci.treefile -s $WD/turtle.fa --scf 100 -seed $SEED -T 1 --prefix $OutDir/turtle.fa.cf"
 
 # link-exchange-rates model
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -m `"MIX{GTR+FO,GTR+FO}`" --link-exchange-rates --prefix test_scripts/test_data/turtle.mix.link -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -m `"MIX{GTR+FO,GTR+FO}`" --link-exchange-rates --prefix $OutDir/turtle.mix.link -seed $SEED -T 1"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -m `"MIX{GTR{1,1,1,1,1,1}+FO,GTR{1,1,1,1,1,1}+FO}`" --link-exchange-rates --prefix test_scripts/test_data/turtle.mix.jc.link -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -m `"MIX{GTR{1,1,1,1,1,1}+FO,GTR{1,1,1,1,1,1}+FO}`" --link-exchange-rates --prefix $OutDir/turtle.mix.jc.link -seed $SEED -T 1"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -p test_scripts/test_data/turtle.nex -g test_scripts/test_data/turtle.constr.tree --prefix test_scripts/test_data/turtle.nex.constr -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -p $WD/turtle.nex -g $WD/turtle.constr.tree --prefix $OutDir/turtle.nex.constr -T 1 -seed $SEED"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -p test_scripts/test_data/turtle.nex -g test_scripts/test_data/turtle.constr.tree2 -B 1000 -alrt 1000 --prefix test_scripts/test_data/turtle.nex.constr2 -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -p $WD/turtle.nex -g $WD/turtle.constr.tree2 -B 1000 -alrt 1000 --prefix $OutDir/turtle.nex.constr2 -T 1 -seed $SEED"
 
-Measure-IQTree "$IQTreeBin -s test_scripts/test_data/turtle.fa -m "MIX+MF" --prefix test_scripts/test_data/turtle.mixfinder -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle.fa -m `"MIX+MF`" --prefix $OutDir/turtle.mixfinder -T 1 -seed $SEED"
 
 ## amino acid test cases
-# the data set is a subset of the turtle data set
-echo "Running amino acid test cases..."
+Write-Host "Running amino acid test cases..."
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -B 1000 -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -B 1000 -T 1 -seed $SEED --prefix $OutDir/turtle_aa.fasta"
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -B 1000 -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -p $WD/turtle_aa.nex -B 1000 -T 1 -seed $SEED --prefix $OutDir/turtle_aa.nex"
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -B 1000 -T 1 -m MFP+MERGE -rcluster 10 --prefix test_scripts/test_data/turtle_aa.merge -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -p $WD/turtle_aa.nex -B 1000 -T 1 -m MFP+MERGE -rcluster 10 --prefix $OutDir/turtle_aa.merge -seed $SEED"
 
-Get-Content test_scripts/test_data/turtle_aa.fasta.treefile, test_scripts/test_data/turtle_aa.nex.treefile |
-    Set-Content test_scripts/test_data/turtle_aa.trees
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.merge.best_scheme.nex -z test_scripts/test_data/turtle_aa.trees -zb 10000 -au -n 0 --prefix test_scripts/test_data/turtle_aa.test -seed $SEED -T 1"
+Get-Content "$OutDir/turtle_aa.fasta.treefile", "$OutDir/turtle_aa.nex.treefile" |
+    Set-Content "$OutDir/turtle_aa.trees"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -p $OutDir/turtle_aa.merge.best_scheme.nex -z $OutDir/turtle_aa.trees -zb 10000 -au -n 0 --prefix $OutDir/turtle_aa.test -seed $SEED -T 1"
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex.best_scheme.nex -z test_scripts/test_data/turtle_aa.trees -n 0 -wpl --prefix test_scripts/test_data/turtle_aa.wpl -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -p $OutDir/turtle_aa.nex.best_scheme.nex -z $OutDir/turtle_aa.trees -n 0 -wpl --prefix $OutDir/turtle_aa.wpl -seed $SEED -T 1"
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -S test_scripts/test_data/turtle_aa.nex --prefix test_scripts/test_data/turtle_aa.loci -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -S $WD/turtle_aa.nex --prefix $OutDir/turtle_aa.loci -T 1 -seed $SEED"
 
-Measure-IQTree "$IQTreeBin  -t test_scripts/test_data/turtle_aa.nex.treefile --gcf test_scripts/test_data/turtle_aa.loci.treefile -s test_scripts/test_data/turtle_aa.fasta --scf 100 -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -t $OutDir/turtle_aa.nex.treefile --gcf $OutDir/turtle_aa.loci.treefile -s $WD/turtle_aa.fasta --scf 100 -seed $SEED -T 1 --prefix $OutDir/turtle_aa.nex.cf"
 
-Measure-IQTree "$IQTreeBin  -t test_scripts/test_data/turtle_aa.fasta.treefile --gcf test_scripts/test_data/turtle_aa.loci.treefile -s test_scripts/test_data/turtle_aa.fasta --scf 100 -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -t $OutDir/turtle_aa.fasta.treefile --gcf $OutDir/turtle_aa.loci.treefile -s $WD/turtle_aa.fasta --scf 100 -seed $SEED -T 1 --prefix $OutDir/turtle_aa.fasta.cf"
 
-# link-exchange-rates model
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -m `"MIX{LG+F,WAG+F}`" --prefix $OutDir/turtle_aa.mix -seed $SEED -T 1"
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -m `"MIX{LG+F,WAG+F}`" --prefix test_scripts/test_data/turtle_aa.mix -seed $SEED -T 1"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -p $WD/turtle_aa.nex -g $WD/turtle.constr.tree --prefix $OutDir/turtle_aa.nex.constr -T 1 -seed $SEED"
 
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -g test_scripts/test_data/turtle.constr.tree --prefix test_scripts/test_data/turtle_aa.nex.constr -T 1 -seed $SEED"
-
-Measure-IQTree "$IQTreeBin  -s test_scripts/test_data/turtle_aa.fasta -p test_scripts/test_data/turtle_aa.nex -g test_scripts/test_data/turtle.constr.tree2 -B 1000 -alrt 1000 --prefix test_scripts/test_data/turtle_aa.nex.constr2 -T 1 -seed $SEED"
+Measure-IQTree "$IQTreeBin -s $WD/turtle_aa.fasta -p $WD/turtle_aa.nex -g $WD/turtle.constr.tree2 -B 1000 -alrt 1000 --prefix $OutDir/turtle_aa.nex.constr2 -T 1 -seed $SEED"
