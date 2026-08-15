8452b57187300e2f71666b786391b5f907b9dcfe
Thomas Wong <thomaskf@gmail.com>
2026-08-15 19:18:08 +1000

    Remove -tmix_opt_method from the command line
    -hmmster is the only way a user needs to select the HMM. The option exposed
    four further optimization paths, one of which (hmast) was accepted but absent
    from its own usage message and duplicated the mast optimizer. The dispatch in
    IQTreeMixHmm is unchanged.

--- utils/tools.cpp
+++ utils/tools.cpp
@@ -3323,15 +3323,6 @@ void parseArg(int argc, char *argv[], Params &params) {
                 params.HMM_no_avg_brlen = true;
                 continue;
             }
-            if (strcmp(argv[cnt], "-tmix_opt_method") == 0) {
-                cnt++;
-                if (cnt >= argc)
-                    throw "Use -tmix_opt_method <hmm/hmm2mast/mast2hmm/mast>";
-                params.treemix_optimize_methods = argv[cnt];
-                if (strcmp(argv[cnt], "hmm") != 0 && strcmp(argv[cnt], "hmm2mast") != 0 && strcmp(argv[cnt], "mast") != 0 && strcmp(argv[cnt], "mast2hmm") != 0 && strcmp(argv[cnt], "hmast") != 0)
-                    throw "Wrong value for -tmix_opt_method";
-                continue;
-            }
 			if (strcmp(argv[cnt], "-brent") == 0) {
 				params.optimize_by_newton = false;
 				continue;
